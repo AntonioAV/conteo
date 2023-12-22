a5cb6ded8dc85c591916a59da655bd9f80ac1890
Antonio Arencibia <arencibiacasa@gmail.com>
2023-12-22 18:54:43 +0100

    Segundo internto con Statefull

--- lib/screens/body.dart
+++ lib/screens/body.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class Body extends StatefulWidget {
+  Body({
+    super.key,
+  });
 
+  final List numbers = [];
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return Column(
