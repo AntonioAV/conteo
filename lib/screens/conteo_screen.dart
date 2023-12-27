8c0e88dfb00c1c0c10fb0a749d07d3f361544f9a
Antonio Arencibia <arencibiacasa@gmail.com>
2023-12-27 19:26:54 +0100

    Inicio fichero conteo_provider

--- lib/screens/conteo_screen.dart
+++ lib/screens/conteo_screen.dart
@@ -7,7 +7,31 @@ class ConteoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('Conteo App'),
+      ),
+      body: const BodyScreen(),
+    );
+  }
+}
+
+class BodyScreen extends StatelessWidget {
+  const BodyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Text('     Posición:   $index');
+              },
+            ),
+          )
+        ],
       ),
     );
   }
