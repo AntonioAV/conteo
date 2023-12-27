4ca0176e436d01bd415f91bc74aa9e90ca10d8ad
Antonio Arencibia <arencibiacasa@gmail.com>
2023-12-27 18:22:37 +0100

    Empezar de nuevo

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,27 @@
+import 'package:conteo/screens/conteo_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // final List numbers = [];
+  int number = 0;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Conteo',
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Conteo'),
-        ),
-        body: const SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          // Added this
-          child: Center(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: ConteoScreen(),
     );
   }
 }
---
+++ lib/screens/conteo_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ConteoScreen extends StatelessWidget {
+  const ConteoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Conteo App'),
+      ),
+    );
+  }
+}
